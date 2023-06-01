57156f9a4d4cfab37e1b7d7ba8e02823755b7428
Skaysep <93714338+Skaysep@users.noreply.github.com>
2023-06-02 00:01:16 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+const allCategories = ['all', ...new Set(items.map(item => item.category))];
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = category => {
+    console.log('click', category);
+    if(category =='all'){
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline" />
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+      <div>
+        <footer className="footer">
+      <a href="https://github.com/Skaysep" className="by">
+        By Morozysnkyi Maksym
+      </a>
+    </footer>
+      </div>
+      
+    </main>
+  );
+}
+
+export default App;
